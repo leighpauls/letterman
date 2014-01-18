c89ae59651deac8366add6d4201a9c49273d0607
Leigh Pauls <leigh.pauls@gmail.com>
2014-01-18 16:32:36 -0500

    Break the auto config file namespace up for clarity, and fix a crash related to those names

--- src/ca/teamdave/letterman/auto/modes/ScoreTwoDriving.java
+++ src/ca/teamdave/letterman/auto/modes/ScoreTwoDriving.java
@@ -39,65 +39,79 @@ public class ScoreTwoDriving implements AutoMode {
 
     public AutoCommand getRootCommand() {
         try {
-            PidControllerConfig turnPidConfig = new PidControllerConfig(mConfig.getJSONObject("turnPid"));
-            PidControllerConfig drivePidConfig = new PidControllerConfig(mConfig.getJSONObject("drivePid"));
-            
-            double turnLockDistanceOut = DaveUtils.jsonDouble(mConfig, "turnLockDistance");
-            double completeDistanceOut = DaveUtils.jsonDouble(mConfig, "completeDistance");
-            double turnLockDistanceBack = DaveUtils.jsonDouble(mConfig, "turnLockDistanceBack");
-            double completeDistanceBack = DaveUtils.jsonDouble(mConfig, "completeDistanceBack");
-            double heading = DaveUtils.jsonDouble(mConfig, "heading");
-            double completionErrorAngle = DaveUtils.jsonDouble(mConfig, "completionErrorAngle");
-            double pause = DaveUtils.jsonDouble(mConfig, "pause");
-            double forwardStopSpeed = DaveUtils.jsonDouble(mConfig, "forwardStopSpeed");
-            double turnStopSpeed = DaveUtils.jsonDouble(mConfig, "turnStopSpeed");
-            
-            RobotPosition shootPosition = new RobotPosition(mConfig.getJSONObject("shootPosition"));
-            RobotPosition pickupPosition = new RobotPosition (mConfig.getJSONObject("pickupPosition"));
-            
+            // Common PID configurations
+            PidControllerConfig turnPidConfig = new PidControllerConfig(
+                    mConfig.getJSONObject("turnPid"));
+            PidControllerConfig drivePidConfig = new PidControllerConfig(
+                    mConfig.getJSONObject("drivePid"));
+
+            // Special field positions
+            RobotPosition shootPosition = new RobotPosition(
+                    mConfig.getJSONObject("shootPosition"));
+            RobotPosition pickupPosition = new RobotPosition(
+                    mConfig.getJSONObject("pickupPosition"));
+
+            // Parameters for driving to the shoot positions
+            JSONObject driveOutJson = mConfig.getJSONObject("driveOut");
             DriveToPointConfig driveOut = new DriveToPointConfig(
                     shootPosition,
-                    turnLockDistanceOut,
-                    completeDistanceOut,
+                    DaveUtils.jsonDouble(driveOutJson, "turnLockDistance"),
+                    DaveUtils.jsonDouble(driveOutJson, "completeDistance"),
                     turnPidConfig,
                     drivePidConfig);
+
+            // Parameters for aiming at the goal
+            JSONObject aimingTurnJson = mConfig.getJSONObject("aimingTurn");
+            double heading = DaveUtils.jsonDouble(aimingTurnJson, "heading");
+            double completionErrorAngle = DaveUtils.jsonDouble(
+                    aimingTurnJson,
+                    "completionErrorAngle");
+            double forwardStopSpeed = DaveUtils.jsonDouble(aimingTurnJson, "forwardStopSpeed");
+            double turnStopSpeed = DaveUtils.jsonDouble(aimingTurnJson, "turnStopSpeed");
+
+            // Parameters for driving back to the ball for pickup
+            JSONObject driveBackJson = mConfig.getJSONObject("driveBack");
             DriveToPointConfig driveBackToBall = new DriveToPointConfig(
                     pickupPosition,
-                    turnLockDistanceBack,
-                    completeDistanceBack,
+                    DaveUtils.jsonDouble(driveBackJson, "turnLockDistance"),
+                    DaveUtils.jsonDouble(driveBackJson, "completeDistance"),
                     turnPidConfig,
                     drivePidConfig);
-            
+            double pickupPause = DaveUtils.jsonDouble(mConfig, "pickupPause");
+
             return new Series(new AutoCommand[] {
-                // TODO: drive this construction nicely from somewhere
                 new DriveToPoint(driveOut, mDriveBase),
                 new Latch(new AutoCommand[]{
                     new TurnToHeading(
                             new TurnToHeadingConfig(heading, completionErrorAngle, turnPidConfig),
                             mDriveBase),
-                    new WaitForDriveStopped(new WaitForDriveStoppedConfig(0.1, 5), mDriveBase)
+                    new WaitForDriveStopped(
+                            new WaitForDriveStoppedConfig(forwardStopSpeed, turnStopSpeed),
+                            mDriveBase)
                 }),
                 new StopDrive(mDriveBase),
                 new DummyShoot(),
 
                 // TODO: run pickup here
                 new DriveToPoint(driveBackToBall, mDriveBase),
-                new Pause(pause),
+                new Pause(pickupPause),
                 // TODO: stop running pickup here
 
                 new DriveToPoint(driveOut, mDriveBase),
                 new Latch(new AutoCommand[]{
                     new TurnToHeading(
-                            new TurnToHeadingConfig(heading, completionErrorAngle, turnPidConfig),
+                            new TurnToHeadingConfig(-heading, completionErrorAngle, turnPidConfig),
                             mDriveBase),
-                    new WaitForDriveStopped(new WaitForDriveStoppedConfig(forwardStopSpeed, turnStopSpeed), mDriveBase)
+                    new WaitForDriveStopped(
+                            new WaitForDriveStoppedConfig(forwardStopSpeed, turnStopSpeed),
+                            mDriveBase)
                 }),
                 new StopDrive(mDriveBase),
                 new DummyShoot()
             });
         } catch (JSONException ex) {
             ex.printStackTrace();
-            throw new RuntimeException("FIled to make ScoreTwoDriving");
+            throw new RuntimeException("Failed to make ScoreTwoDriving");
         }
     }
 }
